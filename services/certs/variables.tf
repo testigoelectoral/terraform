6657d3db98ae685352e15ca6c22699f4bfc83873
Andres Herrera <andres.herrera@gmail.com>
2021-12-02 13:36:52 -0500

    Certs: agregando variable de salida y corrigiendo para producción

--- services/certs/variables.tf
+++ services/certs/variables.tf
@@ -2,9 +2,13 @@
 locals {
   domain = "testigoelectoral.org"
   api_domain_environment = {
-    "production" = "api.${local.domain}"
+    "prod" = "api.${local.domain}"
     "dev" = "api-dev.${local.domain}"
     "stage" = "api-stage.${local.domain}"
   }
   api_domain = local.api_domain_environment[var.environment]
+}
+
+output "arn_api" {
+  value = aws_acm_certificate.api.id
 }
