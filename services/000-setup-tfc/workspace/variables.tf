8c401a50f1f083c9cc4894429eadb74d5aacfd39
Andres Herrera <andres.herrera@gmail.com>
2021-12-20 21:06:08 -0500

    Agregando el setup automático de los workspaces en terraform cloud

---
+++ services/000-setup-tfc/workspace/variables.tf
@@ -0,0 +1,18 @@
+
+variable service {
+  type = string
+}
+
+variable key {
+  type = string
+}
+
+variable environment {
+  type = string
+}
+
+locals {
+  org = "testigoelectoral"
+  repo = "testigoelectoral/terraform"
+  oauth = "ot-KWJXQyRwAq71FZaa"
+}
