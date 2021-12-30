a82bae3142cc6a01602e24a7a0c583b7824cf6ab
aherrera <aherrera@twilio.com>
2021-12-30 09:02:28 -0500

    Cognito: [REMPLAZO] Cambios de configuracion y se agrega dominio

--- services/003-cognito/variables.tf
+++ services/003-cognito/variables.tf
@@ -1,16 +1,35 @@
+locals {
+  org    = "testigoelectoral"
+  domain = "testigoelectoral.org"
+}
+
+data "tfe_outputs" "certs" {
+  organization = local.org
+  workspace    = "${var.environment}-001-certs"
+}
+
+data "aws_route53_zone" "testigo" {
+  name         = local.domain
+  private_zone = false
+}
+
+locals {
+  cognito_domain = data.tfe_outputs.certs.values.domain_cognito
+  cognito_cert   = data.tfe_outputs.certs.values.arn_cognito
+}
 
 output "pool_id" {
-    value = aws_cognito_user_pool.testigo.id
+  value = aws_cognito_user_pool.testigo.id
 }
 
 output "pool_arn" {
-    value = aws_cognito_user_pool.testigo.arn
+  value = aws_cognito_user_pool.testigo.arn
 }
 
 output "pool_endpoint" {
-    value = aws_cognito_user_pool.testigo.endpoint
+  value = aws_cognito_user_pool.testigo.endpoint
 }
 
 output "client_id" {
-    value = aws_cognito_user_pool_client.testigo.id
+  value = aws_cognito_user_pool_client.testigo.id
 }
