9a888661ffb9fc45755a740c951949c1fc23c937
Andres Herrera <andres.herrera@gmail.com>
2021-12-31 18:18:42 -0500

    cognito: agregando authorizer

--- services/003-cognito/variables.tf
+++ services/003-cognito/variables.tf
@@ -3,19 +3,25 @@ locals {
   domain = "testigoelectoral.org"
 }
 
-data "tfe_outputs" "certs" {
-  organization = local.org
-  workspace    = "${var.environment}-001-certs"
-}
-
 data "aws_route53_zone" "testigo" {
   name         = local.domain
   private_zone = false
 }
 
+data "tfe_outputs" "certs" {
+  organization = local.org
+  workspace    = "${var.environment}-001-certs"
+}
+
+data "tfe_outputs" "apigw" {
+  organization = local.org
+  workspace    = "${var.environment}-002-api-gateway"
+}
+
 locals {
   cognito_domain = data.tfe_outputs.certs.values.domain_cognito
   cognito_cert   = data.tfe_outputs.certs.values.arn_cognito
+  apigw_id       = data.tfe_outputs.apigw.values.arn_api
 }
 
 output "pool_id" {
@@ -32,4 +38,8 @@ output "pool_endpoint" {
 
 output "client_id" {
   value = aws_cognito_user_pool_client.testigo.id
+}
+
+output "authorizer_id" {
+  value = aws_api_gateway_authorizer.authorizer.id
 }
