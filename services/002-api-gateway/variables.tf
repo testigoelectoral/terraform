3c784afe379a11be77cff0b91e7585f08698f958
Andres Herrera <andres.herrera@gmail.com>
2021-12-31 18:17:16 -0500

    api-gateway: output root_id

--- services/002-api-gateway/variables.tf
+++ services/002-api-gateway/variables.tf
@@ -1,12 +1,12 @@
 
 locals {
-  org = "testigoelectoral"
+  org    = "testigoelectoral"
   domain = "testigoelectoral.org"
 }
 
 data "tfe_outputs" "certs" {
   organization = local.org
-  workspace = "${var.environment}-001-certs"
+  workspace    = "${var.environment}-001-certs"
 }
 
 data "aws_route53_zone" "api" {
@@ -16,10 +16,13 @@ data "aws_route53_zone" "api" {
 
 locals {
   api_domain = data.tfe_outputs.certs.values.domain_api
-  cert   = data.tfe_outputs.certs.values.arn_api
+  cert       = data.tfe_outputs.certs.values.arn_api
 }
 
 output "arn_api" {
   value = aws_api_gateway_rest_api.api.id
 }
 
+output "root_id" {
+  value = aws_api_gateway_rest_api.api.root_resource_id
+}
