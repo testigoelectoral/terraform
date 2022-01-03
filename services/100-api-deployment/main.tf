c0500a9c57dd6662fde368851a3af76bb422f11c
aherrera <aherrera@twilio.com>
2022-01-03 18:40:03 -0500

    API-Deployment: Configuración inicial

---
+++ services/100-api-deployment/main.tf
@@ -0,0 +1,24 @@
+
+resource "aws_api_gateway_deployment" "running" {
+  rest_api_id = local.api_id
+
+  triggers = {
+    redeployment = timestamp()
+  }
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
+resource "aws_api_gateway_stage" "running" {
+  deployment_id = aws_api_gateway_deployment.running.id
+  rest_api_id   = local.api_id
+  stage_name    = var.environment
+}
+
+# resource "aws_api_gateway_base_path_mapping" "mapping" {
+#   api_id      = local.api_id
+#   stage_name  = aws_api_gateway_stage.running.stage_name
+#   domain_name = local.domain_name
+# }
