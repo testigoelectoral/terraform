5dbd527f48053c6f9d1364ff3b381661e66c2080
aherrera <aherrera@twilio.com>
2022-01-03 19:39:50 -0500

    API-Gateway: Eliminando variable de salida de dominio

--- services/002-api-gateway/variables.tf
+++ services/002-api-gateway/variables.tf
@@ -26,8 +26,3 @@ output "arn_api" {
 output "root_id" {
   value = aws_api_gateway_rest_api.api.root_resource_id
 }
-
-output "domain_name" {
-  value     = aws_api_gateway_domain_name.domain.domain_name
-  sensitive = true
-}
