bc2b3a84e7109ef458bc69c9e01525c0efe2d468
Andres Herrera <andres.herrera@gmail.com>
2022-01-05 13:27:03 -0500

    API-Upload: Arreglando permisos de lambda para escribir en el bucket de imágenes

--- services/005-api-upload/variables.tf
+++ services/005-api-upload/variables.tf
@@ -24,6 +24,7 @@ locals {
   artifacts_bucket     = data.tfe_outputs.s3.values.bucket_artifacts
   artifacts_bucket_arn = data.tfe_outputs.s3.values.arn_artifacts
   images_bucket        = data.tfe_outputs.s3.values.bucket_images
+  images_bucket_arn    = data.tfe_outputs.s3.values.arn_images
   apigw_id             = data.tfe_outputs.apigw.values.arn_api
   apigw_root           = data.tfe_outputs.apigw.values.root_id
   cognito_autorizer    = data.tfe_outputs.cognito.values.authorizer_id
