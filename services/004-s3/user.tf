d078c2ad98aac45d5aa6df75f554a726d62d67d0
Andres Herrera <andres.herrera@gmail.com>
2022-01-06 17:56:19 -0500

    Cloudfront: Permisos para invalidar y para public ACL

--- services/004-s3/user.tf
+++ services/004-s3/user.tf
@@ -17,7 +17,7 @@ resource "aws_iam_policy" "artifacts" {
       },
       {
         Effect   = "Allow"
-        Action   = ["s3:ListBucket", "s3:PutObject"]
+        Action   = ["s3:ListBucket", "s3:PutObject", "s3:PutObjectAcl"]
         Resource = ["${aws_s3_bucket.webapp.arn}/*", "${aws_s3_bucket.webapp.arn}"]
       },
     ]
--- services/006-cloudfront/variables.tf
+++ services/006-cloudfront/variables.tf
@@ -24,9 +24,10 @@ data "aws_s3_bucket" "app" {
 }
 
 locals {
-  app_domain   = data.tfe_outputs.certs.values.domain_app
-  app_cert     = data.tfe_outputs.certs.values.arn_app
-  bucket_name  = data.tfe_outputs.s3.values.bucket_webapp
-  bucket_arn   = data.tfe_outputs.s3.values.arn_webapp
-  s3_origin_id = "${var.environment}-s3webapp"
+  app_domain     = data.tfe_outputs.certs.values.domain_app
+  app_cert       = data.tfe_outputs.certs.values.arn_app
+  bucket_name    = data.tfe_outputs.s3.values.bucket_webapp
+  bucket_arn     = data.tfe_outputs.s3.values.arn_webapp
+  user_artifacts = data.tfe_outputs.s3.values.user_artifacts
+  s3_origin_id   = "${var.environment}-s3webapp"
 }
