4390f73986e701ae6b385d2295006e8e58a81833
Andres Herrera <andres.herrera@gmail.com>
2022-01-11 15:47:41 -0500

    ALL: Renombrando directorios

--- services/100-api-deployment/variables.tf
+++ services/100-api-deployment/variables.tf
@@ -3,16 +3,6 @@ locals {
   org = "testigoelectoral"
 }
 
-data "tfe_outputs" "certs" {
-  organization = local.org
-  workspace    = "${var.environment}-001-certs"
-}
-
-data "tfe_outputs" "api" {
-  organization = local.org
-  workspace    = "${var.environment}-002-api-gateway"
-}
-
 locals {
   api_id      = data.tfe_outputs.api.values.arn_api
   domain_name = data.tfe_outputs.certs.values.domain_api
