1f1926052940537972e3705c1c7e74b3b0981b4a
Andres Herrera <andres.herrera@gmail.com>
2022-01-12 14:18:46 -0500

    Cognito: Permisos para ejecución de Lambda

---
+++ services/07-cognito/lambda.tf
@@ -0,0 +1,29 @@
+
+resource "aws_lambda_permission" "lambda_policy" {
+  statement_id  = "AllowExecutionFromCognito"
+  action        = "lambda:InvokeFunction"
+  principal     = "cognito-idp.amazonaws.com"
+  source_arn    = aws_cognito_user_pool.testigo.arn
+  function_name = "be-cognito-user-hash-${var.environment}"
+  qualifier     = "running"
+}
+
+resource "aws_iam_policy" "policy" {
+  name = "lambda-cognito-user-hash-${var.environment}"
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect   = "Allow"
+        Action   = "cognito:AdminUpdateUserAttributes"
+        Resource = "${aws_cognito_user_pool.testigo.arn}"
+      },
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "policy-attach" {
+  role       = "lambda-cognito-user-hash-${var.environment}"
+  policy_arn = aws_iam_policy.policy.arn
+}
