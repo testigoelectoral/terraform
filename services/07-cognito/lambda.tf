ea0d5caeefb254d32cfe467bde2469e3b66ccdac
Andres Herrera <andres.herrera@gmail.com>
2022-01-17 18:31:33 -0500

    Cognito: Policy para la función de validaciones

--- services/07-cognito/lambda.tf
+++ services/07-cognito/lambda.tf
@@ -27,3 +27,32 @@ resource "aws_iam_role_policy_attachment" "policy-attach" {
   role       = "lambda-cognito-user-hash-${var.environment}"
   policy_arn = aws_iam_policy.policy.arn
 }
+
+resource "aws_lambda_permission" "validations" {
+  statement_id  = "AllowExecutionFromCognito"
+  action        = "lambda:InvokeFunction"
+  principal     = "cognito-idp.amazonaws.com"
+  source_arn    = aws_cognito_user_pool.testigo.arn
+  function_name = "backend-cognito-validations-${var.environment}"
+  qualifier     = "running"
+}
+
+resource "aws_iam_policy" "validations" {
+  name = "lambda-cognito-validations-${var.environment}"
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect   = "Allow"
+        Action   = "cognito-idp:AdminUpdateUserAttributes"
+        Resource = "${aws_cognito_user_pool.testigo.arn}"
+      },
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "validations" {
+  role       = "lambda-cognito-validations-${var.environment}"
+  policy_arn = aws_iam_policy.policy.arn
+}
