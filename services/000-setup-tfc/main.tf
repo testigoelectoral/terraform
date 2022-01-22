632d4169990898a9f912fc0936d453a18593b38a
Andres Herrera <andres.herrera@gmail.com>
2022-01-22 11:52:57 -0500

    SetupTFC: Agregando workspaces para dynamodb y api-myimages

--- services/000-setup-tfc/main.tf
+++ services/000-setup-tfc/main.tf
@@ -22,31 +22,3 @@ module "workspace_prod" {
   key         = each.value
   environment = "prod"
 }
-
-
-module "api_deployment_dev" {
-  source      = "./api-deployment"
-  ws_ids      = [for k, v in local.services : module.workspace_dev[k].ws_id if length(regexall("^api-", k)) > 0]
-  environment = "dev"
-  depends_on = [
-    module.workspace_dev
-  ]
-}
-
-module "api_deployment_stage" {
-  source      = "./api-deployment"
-  ws_ids      = [for k, v in local.services : module.workspace_stage[k].ws_id if length(regexall("^api-", k)) > 0]
-  environment = "stage"
-  depends_on = [
-    module.workspace_stage
-  ]
-}
-
-module "api_deployment_prod" {
-  source      = "./api-deployment"
-  ws_ids      = [for k, v in local.services : module.workspace_prod[k].ws_id if length(regexall("^api-", k)) > 0]
-  environment = "prod"
-  depends_on = [
-    module.workspace_prod
-  ]
-}
--- services/000-setup-tfc/outputs.tf
+++ services/000-setup-tfc/outputs.tf
@@ -1,8 +1,8 @@
 
 output "ws_name" {
   value = {
-    "dev"   = { for k in keys(local.services) : k => module.workspace_dev[k].ws_name }
-    "stage" = { for k in keys(local.services) : k => module.workspace_stage[k].ws_name }
-    "prod"  = { for k in keys(local.services) : k => module.workspace_prod[k].ws_name }
+    "dev"   = { for k in keys(local.services) : k => { name = module.workspace_dev[k].ws_name, id = module.workspace_dev[k].ws_id } }
+    "stage" = { for k in keys(local.services) : k => { name = module.workspace_stage[k].ws_name, id = module.workspace_stage[k].ws_id } }
+    "prod"  = { for k in keys(local.services) : k => { name = module.workspace_prod[k].ws_name, id = module.workspace_prod[k].ws_id } }
   }
 }
