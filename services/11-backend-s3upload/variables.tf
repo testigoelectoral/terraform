4ddc8e2125ef775858a8e9302557608b57f45913
Andres Herrera <andres.herrera@gmail.com>
2022-01-25 23:00:46 -0500

    BackendS3Upload: Variables de entorno

--- services/11-backend-s3upload/variables.tf
+++ services/11-backend-s3upload/variables.tf
@@ -9,5 +9,7 @@ locals {
   images_bucket_arn = data.tfe_outputs.s3.values.arn_images
   artifacts_bucket  = data.tfe_outputs.s3.values.bucket_artifacts
   cognito_arn       = data.tfe_outputs.cognito.values.pool_arn
+  cognito_pool_id   = data.tfe_outputs.cognito.values.pool_id
   images_table_arn  = data.tfe_outputs.dynamodb.values.images_arn
+  images_table_name = data.tfe_outputs.dynamodb.values.images_name
 }
