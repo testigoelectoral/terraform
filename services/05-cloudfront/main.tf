9c25521854d061266976e80f336619aa65ff6ab1
Andres Herrera <andres.herrera@gmail.com>
2022-01-27 13:43:26 -0500

    Cloudfront: Aceptando rutas dinámicas -> /index.html

--- services/05-cloudfront/main.tf
+++ services/05-cloudfront/main.tf
@@ -30,6 +30,13 @@ resource "aws_cloudfront_distribution" "app" {
     max_ttl                = 86400
   }
 
+  custom_error_response {
+    error_caching_min_ttl = 10
+    error_code            = 403
+    response_code         = 200
+    response_page_path    = "/index.html"
+  }
+
   restrictions {
     geo_restriction {
       restriction_type = "none"
