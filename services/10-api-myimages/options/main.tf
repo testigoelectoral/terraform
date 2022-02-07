5b80daa32e77402f2320796c96d4007f1158f4e2
Andres Herrera <andres.herrera@gmail.com>
2022-02-06 23:41:21 -0300

    fix

--- services/10-api-myimages/options/main.tf
+++ services/10-api-myimages/options/main.tf
@@ -7,10 +7,10 @@ resource "aws_api_gateway_method" "options" {
 }
 
 resource "aws_api_gateway_integration" "options" {
-  rest_api_id       = var.apigw_id
-  resource_id       = var.resource_id
-  http_method       = "OPTIONS"
-  type              = "MOCK"
+  rest_api_id = var.apigw_id
+  resource_id = var.resource_id
+  http_method = "OPTIONS"
+  type        = "MOCK"
 
   depends_on = [
     aws_api_gateway_method.options
