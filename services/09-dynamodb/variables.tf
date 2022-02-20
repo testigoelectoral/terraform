e13136f2b3a1092c33304d9c4a43ac93af03a3aa
Andres Herrera <andres.herrera@gmail.com>
2022-02-20 18:32:02 -0300

    DynamoDB: Variables de la tabla processing

--- services/09-dynamodb/variables.tf
+++ services/09-dynamodb/variables.tf
@@ -18,3 +18,11 @@ output "image_votes_arn" {
 output "image_votes_name" {
   value = aws_dynamodb_table.image-votes.name
 }
+
+output "image_processing_arn" {
+  value = aws_dynamodb_table.image-processing.arn
+}
+
+output "image_processing_name" {
+  value = aws_dynamodb_table.image-processing.name
+}
