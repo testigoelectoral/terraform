623de1d344cfbe55ba279dc384eb2409ae9a517c
Andres Herrera <herrera.andres@modak.live>
2022-11-24 14:30:28 -0500

    fix

--- services/07-cognito/variables.tf
+++ services/07-cognito/variables.tf
@@ -14,7 +14,7 @@ locals {
   api_domain              = data.tfe_outputs.certs.values.domain_api
   cognito_cert            = data.tfe_outputs.certs.values.arn_cognito
   apigw_id                = data.tfe_outputs.apigw.values.arn_api
-  userhash_arn            = data.tfe_outputs.backend-cognito.values.hash_arn
+  # userhash_arn            = data.tfe_outputs.backend-cognito.values.hash_arn
 }
 
 output "authorizer_id" {
