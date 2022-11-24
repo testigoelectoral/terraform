45a6f95ceb8836857d14435c93634719510c3b67
Andres Herrera <herrera.andres@modak.live>
2022-11-24 13:38:08 -0500

    Eliminando lambda de cognito

--- services/07-cognito/lambda.tf
+++ services/07-cognito/lambda.tf
@@ -1,29 +1,29 @@
 
-resource "aws_lambda_permission" "lambda_policy" {
-  statement_id  = "AllowExecutionFromCognito"
-  action        = "lambda:InvokeFunction"
-  principal     = "cognito-idp.amazonaws.com"
-  source_arn    = aws_cognito_user_pool.account.arn
-  function_name = "backend-cognito-user-hash-${var.environment}"
-  qualifier     = "running"
-}
+# resource "aws_lambda_permission" "lambda_policy" {
+#   statement_id  = "AllowExecutionFromCognito"
+#   action        = "lambda:InvokeFunction"
+#   principal     = "cognito-idp.amazonaws.com"
+#   source_arn    = aws_cognito_user_pool.account.arn
+#   function_name = "backend-cognito-user-hash-${var.environment}"
+#   qualifier     = "running"
+# }
 
-resource "aws_iam_policy" "policy" {
-  name = "lambda-cognito-user-hash-${var.environment}"
+# resource "aws_iam_policy" "policy" {
+#   name = "lambda-cognito-user-hash-${var.environment}"
 
-  policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [
-      {
-        Effect   = "Allow"
-        Action   = "cognito-idp:AdminUpdateUserAttributes"
-        Resource = "${aws_cognito_user_pool.account.arn}"
-      },
-    ]
-  })
-}
+#   policy = jsonencode({
+#     Version = "2012-10-17"
+#     Statement = [
+#       {
+#         Effect   = "Allow"
+#         Action   = "cognito-idp:AdminUpdateUserAttributes"
+#         Resource = "${aws_cognito_user_pool.account.arn}"
+#       },
+#     ]
+#   })
+# }
 
-resource "aws_iam_role_policy_attachment" "policy-attach" {
-  role       = "lambda-cognito-user-hash-${var.environment}"
-  policy_arn = aws_iam_policy.policy.arn
-}
+# resource "aws_iam_role_policy_attachment" "policy-attach" {
+#   role       = "lambda-cognito-user-hash-${var.environment}"
+#   policy_arn = aws_iam_policy.policy.arn
+# }
